4a24a3925ee07cbda1c4ffc82c7061c9dcbc288b
prithivirajsr <kingprithiviraj27@gmail.com>
2021-11-10 17:34:11 +0530

    p2c&c2p done

--- src/app/job-list/job-list.component.ts
+++ src/app/job-list/job-list.component.ts
@@ -1,31 +1,37 @@
-import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
     selector: 'app-job-list',
     templateUrl: './job-list.component.html',
     styleUrls: ['./job-list.component.css']
 })
-export class JobListComponent implements OnChanges{
+export class JobListComponent implements OnChanges {
 
-jobList:Array<any> = [];
-lastElement:string = "";
+    jobList: Array<any> = [];
+    lastElement: string = "";
 
-@Input("job") newJob:string ="";
+    @Input("job") newJob: string = "";
 
-@Output("showJob") displayJob = new EventEmitter<any>();
-    
-ngOnChanges(){
-    if(this.newJob)
-    this.jobList.push(this.newJob);
-    console.log(this.jobList);
-    if(this.jobList.length !== 0){
-    this.lastElement  = this.jobList[this.jobList.length - 1];
-    alert("The last element of array is: " + this.lastElement);
-    this.displayJob.emit(this.jobList);
+    @Output("showJob") displayJob = new EventEmitter<any>();
+    /*constructor(public cd: ChangeDetectorRef) {
+
+    }*/
+    ngOnChanges() {
+        if (this.newJob)
+            this.jobList.push(this.newJob);
+        console.log(this.jobList);
+        if (this.jobList.length !== 0) {
+            this.lastElement = this.jobList[this.jobList.length - 1];
+            alert("The last element of array is: " + this.lastElement);
+
+        }
+        //this.cd.detectChanges();
     }
-}    
-                           
-   
+    ngAfterViewInit() {
+        this.displayJob.emit(this.jobList);
+    }
 
-     
+
+
+
 }
